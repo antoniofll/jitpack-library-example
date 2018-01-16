299dba3684a9adffcae0c271d97222e0bc3167b4
Antonio <antonio.fernandez@experis.es>
2018-01-16 12:16:57 +0100

    Initial Commit

---
+++ app/src/main/java/com/experis/jitpacktest/Vehicle.java
@@ -0,0 +1,21 @@
+package com.experis.jitpacktest;
+
+/**
+ * Created by Antonio on 16/01/2018.
+ */
+
+public class Vehicle {
+    private int name;
+
+    public Vehicle(int name) {
+        this.name = name;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
